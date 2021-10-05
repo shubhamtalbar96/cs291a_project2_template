1cf6a57b45de0b91534da56d48ec3b93bfc91912
Shubham Talbar <91352665+shubhamtalbar96@users.noreply.github.com>
2021-10-05 01:28:48 -0700

    POST verb checks to create a new file in GCS

--- app.rb
+++ app.rb
@@ -1,31 +1,69 @@
 require 'sinatra'
-require 'google/cloud/storage'
-
-storage = Google::Cloud::Storage.new(project_id: 'cs291a')
-bucket = storage.bucket 'cs291project2', skip_lookup: true
 
 get '/' do
-  
 end
 
 get '/files/' do
-  all_files = bucket.files
-  files.all do |file|
-    puts file.name
-  end
-end
+end 
 
-get '/files/{DIGEST}' do
-  all_files = bucket.files
-  
-end
 
+# create a new file
 post '/files/' do
-  require 'pp'
-  PP.pp request
-  "POST\n"
+
+  require 'digest'
+  # for getting buckets
+  require 'google/cloud/storage'
+
+  # check if file is provided or not
+  if !params['file']
+    status 422
+    headers['Content-Type'] = "application/json"
+    return {:message => "File not provided"}.to_json
+  end
+
+  # the file is stored in tempfile
+  file = params['file']["tempfile"]
+
+  # if the file is not provided
+  if !file
+    status 422
+    headers["Content-Type"] = "application/json"
+    return {:message => "File not provided"}.to_json
+  end
+
+  # to get the file size
+  # read content of the file
+  content = file.read
+
+  if content.length > 1048576
+    status 422
+    headers["Content-Type"] = "application/json"
+    return {:message => "File size greather than 1 MB"}.to_json
+  end
+
+  # hashing the content of the file in hex
+  content_hash = Digest::SHA256.hexdigest content
+
+  # create a new file name as per GCS specs
+  file_name = content_hash.insert 2,"/"
+  file_name = content_hash.insert 5,"/"
+
+  # getting storage and bucket
+  storage = Google::Cloud::Storage.new(project_id: 'cs291a')
+  bucket = storage.bucket 'cs291project2', skip_lookup: true
+
+  # check if GCS already has the file
+  server_file = bucket.file file_name, skip_lookup: true
+
+  if server_file.exists?
+    status 409 
+    headers["Content-Type"] = "application/json"
+    return {:message => "File already present"}.to_json
+  end
+
+  bucket.create_file file, file_name, content_type: params["file"]["type"].to_s
+  status 201
+  header["Content-Type"] = "application/json"
+  return {:uploaded => "#{content_hash.split("/").join}"}.to_json
 end
 
-delete '/files/{DIGEST}' do
-
-end
