bd2f53d4f60e13a8ed23368eb85b905c758724ec
Shubham Talbar <91352665+shubhamtalbar96@users.noreply.github.com>
2021-10-04 21:25:42 -0700

    added more HTTP verbs

--- app.rb
+++ app.rb
@@ -1,11 +1,31 @@
 require 'sinatra'
+require 'google/cloud/storage'
+
+storage = Google::Cloud::Storage.new(project_id: 'cs291a')
+bucket = storage.bucket 'cs291project2', skip_lookup: true
 
 get '/' do
-  "Hello World\n"
+  
 end
 
-post '/' do
+get '/files/' do
+  all_files = bucket.files
+  files.all do |file|
+    puts file.name
+  end
+end
+
+get '/files/{DIGEST}' do
+  all_files = bucket.files
+  
+end
+
+post '/files/' do
   require 'pp'
   PP.pp request
   "POST\n"
+end
+
+delete '/files/{DIGEST}' do
+
 end
